5f43d2282f4331dc85d03284da1f6e21d8d3366c
lsoaress <lucassoaresdecarvalho2013@gmail.com>
2022-04-05 22:18:11 -0300

    seção serviços pronta

--- public/js/main.js
+++ public/js/main.js
@@ -25,7 +25,7 @@
      if ($(document).scrollTop() >= $("#sec_sobre").position().top){
         $('.header').css('background-color', '#713708');
         $('.link_sobre').css('text-decoration','underline');
-        $('.link_home').css('tex-decoration-color','#EBFFE1'); 
+        $('.link_sobre').css('tex-decoration-color','#EBFFE1'); 
 
         $('.link_home').css('color','#EBFFE1');        
         $('.link_team').css('color','#EBFFE1');        
@@ -35,6 +35,25 @@
 
         $('.link_home').css('text-decoration','none');
         $('.link_team').css('text-decoration','none');
+        $('.link_serv').css('text-decoration','none');
+        $('.link_sim').css('text-decoration','none');
+    
+        header.style.transition = "0.2s";
+    } 
+
+    if ($(document).scrollTop() >= $("#sec_equipe").position().top){
+        $('.header').css('background-color', '#713708');
+        $('.link_team').css('text-decoration','underline');
+        $('.link_team').css('tex-decoration-color','#EBFFE1'); 
+
+        $('.link_home').css('color','#EBFFE1');        
+        $('.link_team').css('color','#EBFFE1');        
+        $('.link_sobre').css('color','#EBFFE1');        
+        $('.link_sim').css('color','#EBFFE1');        
+        $('.link_serv').css('color','#EBFFE1');  
+
+        $('.link_home').css('text-decoration','none');
+        $('.link_sobre').css('text-decoration','none');
         $('.link_serv').css('text-decoration','none');
         $('.link_sim').css('text-decoration','none');
     
