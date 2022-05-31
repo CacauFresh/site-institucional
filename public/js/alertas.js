8303cedd2c4b1c192b1bbed3a5294234893cf500
Gabriel Maximiano <gabriel.mgomes@sptech.school>
2022-05-31 14:33:31 -0300

    criação do carrossel da tela de alertas

--- public/js/alertas.js
+++ public/js/alertas.js
@@ -1,17 +1,26 @@
-const imgs = document.getElementById("img");
-const img = document.querySelectorAll("#img img");
+var contador = 0;
 
+var slides = [
+'<img style="width:65vw; height:50vh; margin-left:-110%; margin-top:10%; border-radius:10px; transition: 1s;" src="./imgs/Carrosel_sugestoes/1.png" alt="">',
+'<img style="width:65vw; height:50vh; margin-left:-110%; margin-top:10%; border-radius:10px; transition: 1s;" src="./imgs/Carrosel_sugestoes/2.png" alt="">', 
+'<img style="width:65vw; height:50vh; margin-left:-110%; margin-top:10%; border-radius:10px; transition: 1s;" src="./imgs/Carrosel_sugestoes/3.png" alt="">',
+'<img style="width:65vw; height:50vh; margin-left:-110%; margin-top:10%; border-radius:10px; transition: 1s;" src="./imgs/Carrosel_sugestoes/4.png" alt="">',
+'<img style="width:65vw; height:50vh; margin-left:-110%; margin-top:10%; border-radius:10px; transition: 1s;" src="./imgs/Carrosel_sugestoes/5.png" alt="">'
 
-let idx = 0;
-
+];
 function carrossel(){
-    idx++;
 
-    if(idx > img.length -1){
-        idx = 0;
+
+    caixa_carrossel.innerHTML = `${slides[contador]}`
+    
+    contador++;
+
+    if(contador > slides.length -1){
+        contador = 0
     }
-
-    imgs.style.transform = `translateX(${-idx * 500}px)`
+    
 }
 
-setInterval(carrossel, 1800);
+setInterval(function(){
+    carrossel()
+},3000);
